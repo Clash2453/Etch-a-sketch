fe5b0032deffa1889d45d97207cc514037a9d220
Clash2453 <spasmilenkov123@gmail.com>
2022-07-14 13:14:31 +0300

    Add on hover event to color the grid

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 const squares = document.getElementById('amount-slider');
 const canvas = document.getElementById('pixel-container');
-function clearCanvas(){
+const picker = document.getElementById('color_picker');
+let color = picker.value;
+
+picker.addEventListener('input', function(){
+    color =  this.value;
+});
+
+function clearCanvas(e){
     while(canvas.firstChild){
         canvas.removeChild(canvas.firstChild)
     }
 }
+
 clearCanvas();
+
 function populateCanvas(){
     if(canvas.childNodes.length !== 0){
         return;
@@ -15,8 +24,10 @@ function populateCanvas(){
         pixel.classList.add('pixel');
         pixel.style.width = `${100/squares.value}%`;
         pixel.style.height = `${100/squares.value}%`;
+        pixel.addEventListener('mouseover', function(e){
+            if(e.buttons ===1 || e.buttons === 3)
+                this.style.backgroundColor = color; 
+        });
         canvas.appendChild(pixel);
     }
 }
-
-
